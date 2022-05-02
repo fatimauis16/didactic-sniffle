d7b145fd0bab0f7e61e5b6a93eff566b6a96f11a
fatimauis16 <104153671+fatimauis16@users.noreply.github.com>
2022-05-02 11:21:11 +0400

    Create positive.py

---
+++ positive.py
@@ -0,0 +1,7 @@
+limit1=int(input("enter the lower limit of the list"))
+limit2=int(input("Enter the upper limit of the list"))
+for num in range(limit1, limit2 + 1):
+    if num >= 0:
+        print(num, end = " ")
+
+            

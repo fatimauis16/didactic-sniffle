555d3b256f0bd9ffc57868f64364b169c0358f52
fatimauis16 <104153671+fatimauis16@users.noreply.github.com>
2022-05-07 18:04:29 +0400

    Create e-commerce index.py

---
+++ e-commerce index.py
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>products</title>
+  </head>
+  <body>
+    <nav class="navbar navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand">ECOMMERCE</a>
+    <form class="d-flex">
+      
+      <button class="btn btn-outline-success" type="submit">LOGIN</button>
+      <button class="btn btn-outline-success" type="submit">SIGN UP</button>
+    </form>
+  </div>
+</nav>
+      <div class="container">
+      <div class="card-group">
+    <div class="card">
+      <img class="card-img-top"src="https://images.unsplash.com/photo-1526170375885-4d8ecf77b99f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Polaroid Camera</h5>
+        <h6>category:Cameras</h6>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-primary">View Details</button>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top"src="https://images.unsplash.com/photo-1526170286768-b3c80b34b036?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Polaroid Unsplash Camera</h5>
+        <h6>category:Cameras</h6>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-primary">View Details</button>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top"src="https://images.unsplash.com/photo-1526170160160-1a5eb242ab58?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Polaroid Premium Camera</h5>
+        <h6>category:Cameras</h6>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+      </div>
+      <div class="card-footer">
+        <button class="btn btn-primary">View Details</button>
+      </div>
+    </div>
+  </div>
